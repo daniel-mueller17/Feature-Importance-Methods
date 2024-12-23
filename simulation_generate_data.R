092b65d03bc146c6de0a54c09695a54dd1baafc1
Daniel Müller <daniel._mueller@arcor.de>
2024-12-23 17:28:06 +0100

    simulation1

---
+++ simulation_generate_data.R
@@ -0,0 +1,36 @@
+library("iml")
+library("mlr3")
+library("mlr3verse")
+
+set.seed(123)
+
+n = 10000
+
+ntrain = 0.7 * n
+
+### Simulation 1
+# Y = X1 + X2 + X3 + X4 + eps
+
+x1 = rnorm(n)
+x2 = rnorm(n, sd = 0.01)
+x3 = rnorm(n, sd = 0.1)
+x4 = rnorm(n)
+
+y = x1 + x2 + x3 + x4 + rnorm(n, 0.01)
+
+data_sim1 = data.frame(x1 = x1, x2 = x2, x3 = x3, x4 = x4, y = y)
+
+task = TaskRegr$new(id='simulation1', backend=data_sim1, target='y')
+learner = lrn('regr.lm')
+
+train_set = sample(task$nrow, ntrain)
+test_set = setdiff(seq_len(task$nrow), train_set)
+
+learner$train(task, row_ids = train_set)
+learner$model
+
+predictor_test = Predictor$new(learner, data_sim1[test_set,], y='y')
+
+imp_test <- FeatureImp$new(predictor_test,loss = "mae", n.repetitions = 10, compare='difference')
+
+plot(imp_test)
